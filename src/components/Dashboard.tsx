986ad6d4a986a152b011eb4096e91450e6fe7b64
Marek Schwarz <marek.schwarz@designdev.cz>
2021-11-29 13:48:09 +0100

    Init project

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Wrapper = styled.main`
+  background-color: gray;
+  color: white;
+  height: 100vh;
+`;
+
+const Dashboard = () => {
+  return <Wrapper>something</Wrapper>;
+};
+
+export default Dashboard;
---
+++ src/components/DashboardGroupWrapper.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface Props {
+  items: any[];
+}
+
+const DashboardGroupWrapper: FC<Props> = ({ items }) => {
+  return <div></div>;
+};
+
+export default DashboardGroupWrapper;
